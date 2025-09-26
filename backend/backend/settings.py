ac31a2c79b1a82879355dd9e9c81530be3ed56f3
Anthony Dias <anthonysherondias@gmail.com>
2025-09-27 03:27:11 +0530

    add frontend url

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,7 +82,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://quickassist-frontend.onrender.com",
+    "https://quickassist-techspire.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
